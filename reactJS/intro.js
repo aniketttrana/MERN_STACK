6c15f4077352a54d25361ac17c12b66780a77688
anikettrana <anikettrana@gmail.com>
2023-10-01 00:41:35 +0530

    intro

---
+++ reactJS/intro.js
@@ -0,0 +1,60 @@
+/* Reactjs is a javascript library for Building UI componenets developed by facebook
+
+We can use the Components again and again
+with some modifications in it
+
+React community is very- very large
+
+
+**Difference between Framework and Library
+
+The Framework decides how the code should be called, it would define the structure of your project(Like where the js has to be kept, what should be the folder names etc)
+
+Framework has MVC (Model-View-Controller) and builtin functions
+
+
+while if you are using library, you can structure your project any way you want and just use the library to get your stuff done
+
+It has only V (View) we can only create UI
+
+
+
+
+
+ ***********JSX*****************
+ JSX stands for JavaScript XML.It is a syntax extension to JavaScript .JSX allows us to write HTML in React. JSX makes it easier to write and add HTML in React
+
+
+*************npx vs npm***********
+they are the tools
+npm - node package manager will install the package and then executes it.
+// we can find that package globally
+
+npm install -g create-react-app
+create-react-app my-app
+
+
+
+*npx(node package runner) directly executes and then uninstall that package
+If we want to use the package without installing it on computer we can use npx
+// we cant find that package globally
+
+npx create-react-app ./ 
+REACT APP Will be created in existing folder
+
+OR
+npx create-react-app my-app
+It will create my-app folder in the parent folder 
+
+then npm start
+
+
+
+
+
+
+
+
+
+
+*/
